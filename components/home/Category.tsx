824ede2170d38bf3876efbb81613f714ebd46ab8
anhhuy <phamanhhuy22@gmail.com>
2024-04-08 22:24:58 +0700

    bug fixes mobile UI #4

--- components/home/Category.tsx
+++ components/home/Category.tsx
@@ -15,62 +15,64 @@ function Category() {
     };
 
     const [categories, setCategories] = useState([1, 2, 3, 4])
-    return <div className={"xl:px-[120px] md:px-[36px] sm:px-[20px] px-4 category pt-5"}>
-        <div className={"relative my-10 flex justify-center items-center mb-10 mt-12"}>
+    return <div className={"xl:px-[120px] md:px-[36px] sm:px-[20px] px-4 category"}>
+        <div className={"relative my-10 flex justify-center items-center mb-6 mt-12"}>
             <h2 className={"text-xl font-bold md:text-2xl text-center absolute bg-white px-4 z-20"}>Shop by
                 Categories</h2>
             <span className={"absolute w-full h-[1px] bg-[#232323]"}></span>
         </div>
-        <Swiper
-            pagination={pagination}
-            modules={[Grid, Pagination]}
-            slidesPerGroup={2}
-            grid={{
-                rows: 2,
-                fill: "row",
-            }}
-            breakpoints={{
-                640: {
-                    slidesPerView: 2,
-                    spaceBetween: 20,
-                    grid: {
-                        rows: 1,
-                        fill: "row"
-                    }
-                },
-                850: {
-                    slidesPerView: 3,
-                    spaceBetween: 20,
-                    grid: {
-                        rows: 1,
-                        fill: "row"
-                    }
-                },
-                1024: {
-                    slidesPerView: 4,
-                    spaceBetween: 20,
-                    grid: {
-                        rows: 1,
-                        fill: "row"
-                    }
-                },
-            }}
-            slidesPerView={2}
-            spaceBetween={20}
-            className="mySwiper">
-            {categories.map((item, index) => {
-                return <SwiperSlide
-                    className={twMerge(`relative`)}>
-                    <img src="./images/register_img.png" className={"rounded-md"} alt=""/>
-                    <div
-                        className={"bg-white text-black rounded-lg absolute bottom-4 w-[110px] lg:w-[150px] left-[50%] translate-x-[-50%]"}>
+        <div className={"pt-4"}>
+            <Swiper
+                pagination={pagination}
+                modules={[Grid, Pagination]}
+                slidesPerGroup={2}
+                grid={{
+                    rows: 2,
+                    fill: "row",
+                }}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 20,
+                        grid: {
+                            rows: 1,
+                            fill: "row"
+                        }
+                    },
+                    850: {
+                        slidesPerView: 3,
+                        spaceBetween: 20,
+                        grid: {
+                            rows: 1,
+                            fill: "row"
+                        }
+                    },
+                    1024: {
+                        slidesPerView: 4,
+                        spaceBetween: 20,
+                        grid: {
+                            rows: 1,
+                            fill: "row"
+                        }
+                    },
+                }}
+                slidesPerView={2}
+                spaceBetween={20}
+                className="mySwiper">
+                {categories.map((item, index) => {
+                    return <SwiperSlide
+                        className={twMerge(`relative`)}>
+                        <img src="./images/register_img.png" className={"rounded-md"} alt=""/>
+                        <div
+                            className={"bg-white text-black rounded-lg absolute bottom-4 w-[110px] lg:w-[150px] left-[50%] translate-x-[-50%]"}>
                         <span className={"text-sm w-full text-center block py-1"}>
                             Kids Wear
                         </span>
-                    </div>
-                </SwiperSlide>
-            })}
-        </Swiper>
+                        </div>
+                    </SwiperSlide>
+                })}
+            </Swiper>
+        </div>
     </div>
 }
 
