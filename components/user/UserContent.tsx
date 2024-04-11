5118b0206e8285f4aa427b533e4562e4dfb391c5
anhhuy <phamanhhuy22@gmail.com>
2024-04-12 00:13:22 +0700

    bug fix UI mobile

--- components/user/UserContent.tsx
+++ components/user/UserContent.tsx
@@ -2,7 +2,7 @@ import UserInfomation from "@/components/user/UserInfomation";
 
 function UserContent() {
     return <div className={"mt-[80px] xl:px-[120px] md:px-[36px] sm:px-[20px] px-4"}>
-        <h3>Home {">"} <span className={"text-gray-400"}>Your cart</span></h3>
+        <h3>Home {">"} <span className={"text-gray-400"}>My profile</span></h3>
         <h2 className={"text-xl font-bold mt-4"}>MY PROFILE</h2>
         <UserInfomation/>
     </div>
--- components/user/UserInfomation.tsx
+++ components/user/UserInfomation.tsx
@@ -18,35 +18,31 @@ function UserInfomation() {
         </div>
         <div className={"mt-6"}>
             <div className="flex items-center gap-x-4">
-                <div className="mb-4">
+                <div className="mb-4 w-[50%]">
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="firstName">
                         First Name
                     </label>
-                    <input id="firstName" required type="text" value={"Robert"} disabled
-                           className="px-4 py-2 rounded-[10px] border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4"}>Robert</p>
                 </div>
                 <div className="mb-4 flex-1">
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="lastName">
                         Last Name
                     </label>
-                    <input id="lastName" required type="text" value={"Ford"} disabled
-                           className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4 "}>Ford</p>
                 </div>
             </div>
             <div className="flex items-center gap-x-4">
-                <div className={"w-[80%]"}>
+                <div className={"w-[70%]"}>
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="email">
                         Email Address
                     </label>
-                    <input id="email" required type="email" value={"example@gmail.com"} disabled
-                           className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4"}>exaple@gmail.com</p>
                 </div>
-                <div>
+                <div className={"flex-1"}>
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="email">
                         Gender
                     </label>
-                    <input id="gender" required type="email" value={"Female"} disabled
-                           className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4"}>Female</p>
                 </div>
             </div>
             <div className="flex items-center gap-x-4 mt-4">
@@ -54,23 +50,20 @@ function UserInfomation() {
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="email">
                         Date Of Birth
                     </label>
-                    <input id="birth" required type="email" value={"07/13/2003"} disabled
-                           className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4"}>07/13/2003</p>
                 </div>
-                <div>
+                <div className={"flex-1"}>
                     <label className="block mb-1 text-sm text-gray-500" htmlFor="email">
                         Phone
                     </label>
-                    <input id="gender" required type="phone" value={"0905369675"} disabled
-                           className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                    <p className={"border border-black rounded-lg py-2 px-4"}>0905369675</p>
                 </div>
             </div>
             <div className={"mt-4"}>
                 <label className="block mb-1 text-sm text-gray-500" htmlFor="email">
                     Address
                 </label>
-                <input id="gender" required type="phone" value={"2464 Royal Ln, Mesa, New Jerry 45463"} disabled
-                       className="px-4 py-2 rounded-[10px] w-full border border-black disabled:bg-white"/>
+                <p className={"border border-black rounded-lg py-2 px-4"}>2464 Royal Ln, Mesa, New Jerry 45463</p>
             </div>
         </div>
     </div>
